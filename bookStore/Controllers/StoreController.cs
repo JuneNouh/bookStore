8819b7f18c7452f5bab83e5fbd55f9b9a07e9f02
June Nouh <june.nouh@outlook.com>
2022-06-22 20:07:30 +0200

    Add Images and details page with Design

---
+++ bookStore/Controllers/StoreController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using bookStore.Data;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace bookStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public StoreController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Book.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+    }
+}
