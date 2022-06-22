6b5ed04067128330ffd97ef8277d62eff738983c
June Nouh <june.nouh@outlook.com>
2022-06-22 15:19:27 +0200

    Add project files.

---
+++ bookStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using bookStore.Models;
+
+namespace bookStore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<bookStore.Models.Book> Book { get; set; }
+    }
+}
